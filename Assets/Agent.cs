842e314b4ffbbf288616ad71e8ba6b491c8b1fbf
Antonio <antonio.ruggiero93@hotmail.it>
2018-03-31 16:16:06 +0200

    NavMeshAgent che segue un gameObject

---
+++ Assets/Agent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Agent : MonoBehaviour
+{
+	private NavMeshAgent agent;
+
+	public Transform followObject;
+	
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		this.FixedUpdateAsObservable().SubscribeOnMainThread().Subscribe(unit =>
+			{
+				Debug.Log("Update Destination");
+				agent.SetDestination(followObject.position);
+			});
+	}
+}
