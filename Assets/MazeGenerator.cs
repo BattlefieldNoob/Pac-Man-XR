a6bb8737b918d03e81f2e2485cf3b7f0f2859fa9
Antonio <antonio.ruggiero93@hotmail.it>
2018-03-31 15:47:11 +0200

    aggiunti plugin UniRx e Zenject, script generazione random mappa

---
+++ Assets/MazeGenerator.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MazeGenerator : MonoBehaviour
+{
+	private const float STANDARD_HEIGHT = 0.5f;
+
+	public GameObject CubePrefab;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		createMap(10, 10);
+  
+	}
+ 
+	// Update is called once per frame
+	void Update () {
+  
+	}
+
+	
+	//Ho reso la funzione non statica, altrimenti non potevo usare la variabile "CubePrefab" (non potevo farla statica perchè unity non 
+	//mostra le variabili statiche nell'inspector)
+	private void createMap(int x, int z)
+	{
+		var points = new List<Vector3>();
+  
+		for (int i = 0; i < x; i++)
+		{
+			// Random points
+			points.Add(new Vector3(Random.Range(0, x), STANDARD_HEIGHT, Random.Range(0, z)));
+		}
+  
+		// borders
+		for (int i = 0; i < x; i++)
+		{
+			points.Add(new Vector3(i, STANDARD_HEIGHT, 0f));
+			points.Add(new Vector3(i, STANDARD_HEIGHT, z));
+		}
+
+		for (int i = 0; i < z; i++)
+		{
+			points.Add(new Vector3(0f, STANDARD_HEIGHT, i));
+			points.Add(new Vector3(x, STANDARD_HEIGHT, i));
+		}
+
+		var supportPoints = new List<Vector3>();
+
+		for (int xi = 0; xi < x; xi++)
+		{
+			for (int zi = 0 ; zi < z; zi++)
+			{
+				if (!points.Union(supportPoints).ToList().Exists(it => (it.x + 2 == xi || it.x - 2 == xi) && (it.z - 2 == zi || it.z + 2 == zi)))
+				{
+					supportPoints.Add(new Vector3(xi, STANDARD_HEIGHT, zi));
+				}
+			}
+		}
+  
+		// generate things
+		foreach (var vector3 in points.Union(supportPoints))
+		{
+			//il prefab ha già la scala corretta e viene instanziato nella posizione giusta
+			Instantiate(CubePrefab, vector3, Quaternion.identity);
+			
+			/*	var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+				cube.transform.position = new Vector3(vector3.x, vector3.y, vector3.z);
+				cube.transform.localScale = new Vector3(1, 2, 1);*/
+		}
+	}
+}
---
+++ Assets/NoisePlane.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class NoisePlane : MonoBehaviour
+{
+	private MeshRenderer _renderer;
+	public float scale = 1;
+	
+	private List<Vector2> curves=new List<Vector2>();
+
+	private Texture2D texture;
+
+	public GameObject obj;
+	void Start ()
+	{
+		_renderer = GetComponent<MeshRenderer>();
+	    texture=new Texture2D(10,10);
+		texture.filterMode = FilterMode.Point;
+		
+		foreach (var x in Enumerable.Range(0,texture.width))
+		{
+			foreach (var y in Enumerable.Range(0,texture.height))
+			{
+				var value=Mathf.PerlinNoise((float)x/(texture.width*scale), (float)y/(texture.height*scale));
+				if (value < 0.2f)
+				{
+					//Debug.Log($"i:{x},i1:{y}, value:{value}");
+					texture.SetPixel(x, y, Color.blue);
+					var point=new Vector2(x,y);
+					if (!curves.Any(vector2 => Vector2.Distance(vector2, point) < 2))
+					{
+						curves.Add(point);
+					}
+				}
+				else
+				{
+
+					texture.SetPixel(x, y, new Color(value, value, value));
+				}
+			}
+		}
+		texture.Apply();
+
+		_renderer.material.mainTexture = texture;
+		Debug.Log($"points:{curves.Count}");
+		PositionateCurves();
+	}
+
+
+
+	void PositionateCurves()
+	{
+		foreach (var curve in curves)
+		{
+			Debug.Log($"x:{curve.x}, y:{curve.y}");
+
+			if (curve.x > texture.width / 2)
+			{
+				if (curve.y > texture.height / 2)
+				{
+					Instantiate(obj, new Vector3((-curve.x+5)+0.5f,0,(-curve.y+5)+0.5f), Quaternion.identity);
+				}
+			}
+		}
+	}
+}
